fa8e5f850cf81f77ad838caa9bc898f19f808c0b
Kevin Cochran <kevin@onsiteiq.io>
2020-11-05 12:44:37 -0500

    Add Cypress

---
+++ cypress/integration/button.spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+describe('Blue button', () => {
+    it('Changes color to blue', () => {
+        cy.visit('http://localhost:3000/')
+    })
+})
+
+describe('Red button', () => {
+    it('Change color to red', () => {
+        cy.visit('http://localhost:3000/')
+    })
+})
